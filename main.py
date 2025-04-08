15a205a380cc3f040bfb725defe2f19c3dde5976
v0lt99 <voltflare3@gmail.com>
2025-04-08 15:50:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 pip install flask
 <form action="http://localhost:5000/process" method="post">
-    <input type="text" name="user_input">
+    <input type="HTML" name="Volt-Site">
     <button type="submit">Submit</button>
 </form>
 print("launched successfuly!")
