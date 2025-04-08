0133bd5d711f8ce0f7d806fad4b8251829cc224b
v0lt99 <voltflare3@gmail.com>
2025-04-08 15:48:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+pip install flask
+<form action="http://localhost:5000/process" method="post">
+    <input type="text" name="user_input">
+    <button type="submit">Submit</button>
+</form>
 print("launched successfuly!")
