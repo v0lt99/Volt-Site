69e5cfa56c1ab03dd382ceb38b2d73766dbaac54
v0lt99 <voltflare3@gmail.com>
2025-04-08 15:58:44 +0100

    Delete main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__Volt-Site__)
 
 @app.route("/")
-def home():
+def home():##
     return "<h1>Hello, this is Python sending text to the web!</h1>"
 
 if __name__ == "__main__":
